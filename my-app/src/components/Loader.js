851408627a51f5810b9d45021f4385475aa6b5ed
Mohammad Tahmasebi <mohammad.tahmasebi@students.codeinstitute.net>
2024-08-18 19:02:34 +0100

    Create Loader.js
    Create Loader.js to be called by the Content.js

---
+++ my-app/src/components/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import css from './css/Loader.modules.css'
+
+function Loader() {
+
+  return (
+    <div className='Loader'>
+    </div>
+  )
+}
+
+export default Loader
